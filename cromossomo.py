1cf537a4452046d067300c6367a8e20956a6213a
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2021-08-03 08:46:27 -0300

    Delete cromossomo.py

--- cromossomo.py
+++
@@ -1,8 +0,0 @@
-class Cromossomo:
-
-  def __init__(self, config):
-    self.config = config
-
-  def __str__(self):
-    to_string = 'Inteiro: ' + str(self.config['inteiro']) + ': ' + 'Binario: '+str(self.config['binario']) + ','
-    return to_string + '\n'
