08edc54b60e759a2f964ac43c97bd4bcfc0554a7
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2021-08-03 08:47:10 -0300

    att

--- individual.py
+++ individual.py
@@ -5,23 +5,9 @@ from cromossomo import Cromossomo
 
 class Individual:
 
-  def __init__(self,cromossomo = None):
-    if cromossomo is None:
-      self.cromossomo = []
-      for i in range(20):
-        config = {
-        }
-        aleatorio = np.random.randint(-13000, 13000)
-        config['inteiro'] = aleatorio
-        config['binario'] = bin(aleatorio)
-        config['score'] = 100000
-        cromossomo = Cromossomo(config)
-        self.cromossomo.append(cromossomo)
-    else:
-      self.cromossomo = cromossomo
+  def __init__(self,inteiro):
+    self.score = 10000000
+    self.binary = bin(inteiro)
 
   def __str__(self) :
-    cromossomo = ''
-    for i in range(len(self.cromossomo)):
-      cromossomo += str(self.cromossomo[i])
-    return cromossomo + '\n'
+    return str(self.binary)
--- main.py
+++ main.py
@@ -13,55 +13,109 @@ from datetime import datetime
 
 population = []
 roullete = []
+raizes = []
 newpopulation = []
 coeficiente =[2,11,11,5,9,2,6,3,12,7,9,8,3,2,4,3,1,7,8,7,3]
-score = 0
 
+def selection():
+    qualquerLista = population + newpopulation
+    qualquerLista = sorted(qualquerLista, key=lambda ind: abs(ind.score), reverse=True)
+    selected_population = []
+    for i in range(20):
+        selected_population.append(qualquerLista[i])
+    return selected_population
+
+def mutation():
+    population_mutation = 2
+    for i in range(population_mutation):
+        if len(newpopulation) != 0:
+            ind_index = np.random.randint(0, len(newpopulation) - 1)
+            individual = newpopulation[ind_index]
+            pos = np.random.randint(3,20)
+            if individual.binary[pos] == 0:
+                individual.binary = individual.binary[:pos-1] + "1" + individual.binary[pos+1:]
+            else:
+                individual.binary = individual.binary[:pos - 1] + "0" + individual.binary[pos + 1:]
+
+
+def cruzamentoParte2(pai,mae):
+    part1 = np.random.randint(3,20)
+    individuo1 = Individual(int(pai.binary[:part1] + mae.binary[part1:],2))
+    individuo2 = Individual(int(mae.binary[:part1] + pai.binary[part1:],2))
+    newpopulation.append(individuo1)
+    newpopulation.append(individuo2)
+
+def cruzamentoParte1():
+    for i in range(10):
+        aleatorio1 = np.random.randint(0,len(roullete)-1)
+        aleatorio2 = np.random.randint(0,len(roullete)-1)
+        while aleatorio1 == aleatorio2:
+            aleatorio1 = np.random.randint(0, 100)
+            aleatorio2 = np.random.randint(0, 100)
+
+        pai = roullete[aleatorio1]
+        mae = roullete[aleatorio2]
+        cruzamentoParte2(pai, mae)
 
 def list_as_roulette(soma_total):
-    print("ENTREI AQUI")
+    #print("ENTREI AQUI")
     for ind in population:
         prop = round((ind.score*100)/soma_total)
         for j in range(prop):
             roullete.append(ind)
-    print(len(roullete))
+    #print(len(roullete))
 
-def score():
+def score(population):
     for i in range(len(population)):
-        cromossomo = population[i].cromossomo
-        #print(cromossomo)
-        for j in range(len(cromossomo)):
+        cont = 0
+        seq = 0
+        inteiro = 0
+        for coe in coeficiente:
+            if seq ==0:
+                cont += coe
+            else:
+                inteiro = int(population[i].binary,2)
+                cont += (inteiro**seq) * coe
+                #print(inteiro)
+            seq += 1
+        population[i].score = cont
+        if cont == 0 and population[i].binary not in raizes:
+            raizes.append(population[i].binary)
+            ind_aleatorio = np.random.randint(-1000000,0)
+            population[i].binary = bin(ind_aleatorio)
             cont = 0
             seq = 0
-            inteiro = 0
-            config = cromossomo[j].config
             for coe in coeficiente:
-                if seq ==0:
+                if seq == 0:
                     cont += coe
                 else:
-                    inteiro = config['inteiro']
-                    cont += (inteiro**seq) * coe
-                    #print(inteiro)
+                    inteiro = int(population[i].binary, 2)
+                    cont += (inteiro ** seq) * coe
+                    # print(inteiro)
                 seq += 1
-            config['score'] = cont
-            print(cont)
+            population[i].score = cont
+        #print(cont)
 
 def init_population():
-  for i in range(1):
-    individual = Individual()
+  for i in range(20):
+    aleatorio = np.random.randint(-1000000,0)
+    individual = Individual(aleatorio)
     population.append(individual)
 
 init_population()
-for ind in population:
-    print(ind)
-score()
-#
-# soma_total = 0
-# for ind in population:
-#     soma_total+= ind.score
-# list_as_roulette(soma_total)
-# s = []
-# for m in range(20):
-#     r = np.random.randint(0,len(roullete)-1)
-#     s = roullete[r]
-#     print(s)
+while True:
+    for ind in raizes:
+        print(ind)
+    if len(raizes) == 20:
+        break
+    score(population)
+    soma_total = 0
+    for ind in population:
+        soma_total+= ind.score
+    list_as_roulette(soma_total)
+    media = soma_total / 20
+    cruzamentoParte1()
+    mutation()
+    score(newpopulation)
+    population = selection()
+    newpopulation.clear()
